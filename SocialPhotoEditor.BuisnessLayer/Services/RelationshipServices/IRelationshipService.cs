3a0f29ec0e5bde349597f3315e836c6813c43943
tanya <t.evtushenko.s@yandex.by>
2017-05-10 13:26:24 +0300

    something another

--- SocialPhotoEditor.BuisnessLayer/Services/RelationshipServices/IRelationshipService.cs
+++ SocialPhotoEditor.BuisnessLayer/Services/RelationshipServices/IRelationshipService.cs
@@ -1,4 +1,7 @@
-﻿namespace SocialPhotoEditor.BuisnessLayer.Services.RelationshipServices
+﻿using System.Collections.Generic;
+using SocialPhotoEditor.BuisnessLayer.ViewModels.UserViewModels;
+
+namespace SocialPhotoEditor.BuisnessLayer.Services.RelationshipServices
 {
     public interface IRelationshipService
     {
@@ -7,5 +10,13 @@
         int GetSubscribersCount(string userName);
 
         int GetSubscriptionsCount(string userName);
+
+        void Subscribe(string followerName, string userName);
+
+        void Unsubscribe(string followerName, string userName);
+
+        IEnumerable<UserMinInfoViewModel> GetSubscribers(string userName);
+
+        IEnumerable<UserMinInfoViewModel> GetSubscriptions(string userName);
     }
 }
---
+++ SocialPhotoEditor/Controllers/RelationshipWebApiController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using SocialPhotoEditor.BuisnessLayer.Services.RelationshipServices;
+using SocialPhotoEditor.BuisnessLayer.Services.RelationshipServices.Implementations;
+using SocialPhotoEditor.BuisnessLayer.ViewModels.UserViewModels;
+
+namespace SocialPhotoEditor.Controllers
+{
+    public class RelationshipWebApiController : ApiController
+    {
+        private static readonly IRelationshipService Service =  new RelationshipService();
+
+        public IEnumerable<UserMinInfoViewModel> Post(string userName)
+        {
+            return Service.GetSubscribers(userName);
+        }
+
+        public void Put(string userName)
+        {
+            Service.Subscribe(User.Identity.Name, userName);
+        }
+
+        public void Delete(string userName)
+        {
+            Service.Unsubscribe(User.Identity.Name, userName);
+        }
+    }
+}
