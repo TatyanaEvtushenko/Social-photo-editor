6c8d224af9cf129b510c83c5e5d78c60be7f0ccf
tanya <t.evtushenko.s@yandex.by>
2017-05-22 22:54:57 +0300

    some fixes in uncorrect branch (page with news)

--- SocialPhotoEditor/Scripts/Angular/news/news.controller.js
+++ SocialPhotoEditor/Scripts/Angular/news/news.controller.js
@@ -6,5 +6,23 @@
         }, function (error) {
             console.log("Error from server! (news)");
         });
+
+        $(document).ready(function () {
+            $("#commentInput").emojioneArea({
+                events: {
+                    keypress: function (editor, event) {
+                        if (event.which === 13 || event.keyCode === 13) {
+                            addComment();
+                        }
+                    },
+                    focus: function (editor, event) {
+                        var pos = editor.text().length;
+                        console.log(editor);
+                        editor.select(pos);
+                    }
+                }
+            });
+            $("#commentInput").data("emojioneArea").setFocus(true);
+        });
     }
 ]);
